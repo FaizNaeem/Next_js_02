05ed4744260568858fd0597720197b66724cb233
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-24 18:50:18 +0500

    continue

--- src/blog/[id]/page.js
+++ src/blog/[id]/page.js
@@ -1,7 +1,11 @@
-import getUser from '@/app/lib/getUser'
+import getUserPost from '@/app/lib/getUserPost'
+import getUserDetail from '@/app/lib/getUserDetails'
 import Link from 'next/link'
-export default async function IdPage() {
-    const get = await getUser()
+import getUserDetail from '@/app/lib/getUserDetails'
+export default async function IdPage({params}) {
+    const getUserDetail = await getUserDetail(params.id)
+    const getUserPost = await getUserPost(params.id)
+    const [user] = await Promise.all([getUserDetail])
   return (
     <div className='flex min-h-screen flex-col justify-between p-23'>
         <h1 className='text-center font-bold text-2xl'>Users</h1>
