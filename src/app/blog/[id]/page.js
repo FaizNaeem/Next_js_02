2c5116c57b84d04e24af5b95560355d313504690
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-24 19:12:49 +0500

    continue

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,11 +1,10 @@
 import getUserPost from '@/app/lib/getUserPost'
-import getUserDetail from '@/app/lib/getUserDetails'
 import Link from 'next/link'
 import getUserDetail from '@/app/lib/getUserDetails'
 export default async function IdPage({params}) {
-    const getUserDetail = await getUserDetail(params.id)
+    const getUserDetail1 = await getUserDetail(params.id)
     const getUserPost = await getUserPost(params.id)
-    const [user] = await Promise.all([getUserDetail])
+    const [user] = await Promise.all([getUserDetail1])
   return (
     <div className='flex min-h-screen flex-col justify-between p-23'>
         <h1 className='text-center font-bold text-2xl'>Users</h1>
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -6,9 +6,9 @@ export default async function Show() {
     <div className='flex min-h-screen flex-col justify-between p-23'>
         <h1 className='text-center font-bold text-2xl'>Users</h1>
         {
-            get.map((e,id)=>{
+            get.map((e,)=>{
 return(
-    <Link href={`/blog/${id}`}>
+    <Link href={`blog/${e.id}`}>
     <div className='p-5 w-full m-4 bg-blue-300 rounded-md'>
         <h1 className='font-bold border'>{e.name}</h1>
 <p>{e.email}</p>
