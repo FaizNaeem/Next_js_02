36bde989ba8eb7a7c22a31d35c3d64e8af7778dc
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-25 20:24:15 +0500

    change meta tag

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -4,6 +4,13 @@ import getUserDetail from '@/app/lib/getUserDetails'
 import UserPost from '@/com/UserPost'
 import './loader.css'
 import { Suspense } from 'react'
+export async function generateMetadata({ params }){
+  const getUserDetail1 = await getUserDetail(params.id);
+  return{
+    title : getUserDetail1.name,
+    description : getUserDetail1.email,
+  }
+}
 export default async function IdPage({params}) {
     const getUserDetail1 =  getUserDetail(params.id)
     const userpost =  getUserPost(params.id)
@@ -13,7 +20,7 @@ export default async function IdPage({params}) {
     return (
     <div className='flex min-h-screen flex-col justify-between p-23'>
        <div>
-        <h2 className='text-center text-2xl font-semibold p-3 mt-3 bg-gray-300'>User name : {user.name}</h2>
+        <h2 className='text-center text-2xl font-semibold p-3 mt-3 bg-gray-300 sticky'>User name : {user.name}</h2>
         <h2 className='text-center text-2xl font-semibold p-3  bg-gray-300'>User email : {user.email}</h2>
         <Suspense fallback={<div style={{display:"flex" , alignItems:"center" , justifyContent:"center", width:"100%", height:"50vh"}}>
 <div className="loader"></div>
